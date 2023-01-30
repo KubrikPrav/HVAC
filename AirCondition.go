ba270028744a36cf7356917403056f4ce06018b8
Nikita <90956399+nikitagriaznov@users.noreply.github.com>
2023-01-30 14:20:31 +0300

    airConrition now visible

--- AirCondition.go
+++ AirCondition.go
@@ -2,31 +2,31 @@ package HVAC
 
 import "errors"
 
-type airCondition struct {
+type Air struct {
 	Temperature float64 // in ºC
 	Humidity    float64 // in g/kg
 }
 
-func (s airCondition) VapourPressure() float64 {
+func (s Air) VapourPressure() float64 {
 	return VaporPressure(s.Temperature)
 }
-func (s airCondition) VapourDensity() float64 {
+func (s Air) VapourDensity() float64 {
 	return VaporDensity(s.Temperature)
 }
-func (s airCondition) AirDensity() float64 {
+func (s Air) AirDensity() float64 {
 	return AirDensity(s.Temperature)
 }
-func (s *airCondition) SetHumidityRatio(HumidityRatio float64) error {
+func (s *Air) SetHumidityRatio(HumidityRatio float64) error {
 	if HumidityRatio < 0 {
 		return errors.New("invalid Humidity Ratio")
 	}
 	s.Humidity = HumidityRatio * s.VapourDensity() / s.AirDensity()
 	return nil
 }
-func (s airCondition) HumidityRatio() float64 {
+func (s Air) HumidityRatio() float64 {
 	return s.Humidity * s.AirDensity() / s.VapourDensity()
 }
 
-func (s airCondition) PartialPressure() float64 {
+func (s Air) PartialPressure() float64 {
 	return PartialPressure(s.Temperature, s.HumidityRatio())
 }
