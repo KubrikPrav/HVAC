a17cd449c0df41e48b24b866a3ac7529efd55d92
Nikita <90956399+nikitagriaznov@users.noreply.github.com>
2023-01-31 12:28:41 +0300

    Humidity fix

--- AirCondition.go
+++ AirCondition.go
@@ -20,11 +20,11 @@ func (s *Air) SetHumidityRatio(HumidityRatio float64) error {
 	if HumidityRatio < 0 {
 		return errors.New("invalid Humidity Ratio")
 	}
-	s.Humidity = HumidityRatio * s.VapourDensity() / s.AirDensity()
+	s.Humidity = (HumidityRatio / 100) * s.VapourDensity() / s.AirDensity()
 	return nil
 }
 func (s Air) HumidityRatio() float64 {
-	return s.Humidity * s.AirDensity() / s.VapourDensity()
+	return s.Humidity * 100 * s.AirDensity() / s.VapourDensity()
 }
 
 func (s Air) PartialPressure() float64 {
