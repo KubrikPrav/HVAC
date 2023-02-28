05f7f85d0e0b5e02c898a6ad0790f2b26bddac77
Nikita <90956399+nikitagriaznov@users.noreply.github.com>
2023-02-28 13:38:12 +0300

    PrintAir

--- AirCondition.go
+++ AirCondition.go
@@ -1,19 +1,36 @@
 package HVAC
 
-import "errors"
+import (
+	"errors"
+	"math"
+)
 
 type Air struct {
 	Temperature float64 // in ºC
 	Humidity    float64 // in g/kg
 }
+type PrintAir struct {
+	Temperature   float64
+	HumidityRatio float64
+}
 
-func (s Air) VapourPressure() float64 {
+func (s *PrintAir) Air() (out Air) {
+	out.Temperature = s.Temperature
+	out.SetHumidityRatio(s.HumidityRatio)
+	return
+}
+
+func (s *Air) PrintAir(digits int) (out PrintAir) {
+	return PrintAir{math.Round(s.Temperature*math.Pow10(digits)) / math.Pow10(digits), math.Round(s.HumidityRatio()*math.Pow10(digits)) / math.Pow10(digits)}
+}
+
+func (s *Air) VapourPressure() float64 {
 	return VaporPressure(s.Temperature)
 }
-func (s Air) VapourDensity() float64 {
+func (s *Air) VapourDensity() float64 {
 	return VaporDensity(s.Temperature)
 }
-func (s Air) AirDensity() float64 {
+func (s *Air) AirDensity() float64 {
 	return AirDensity(s.Temperature)
 }
 func (s *Air) SetHumidityRatio(HumidityRatio float64) error {
@@ -23,10 +40,10 @@ func (s *Air) SetHumidityRatio(HumidityRatio float64) error {
 	s.Humidity = (HumidityRatio / 100) * s.VapourDensity() / s.AirDensity()
 	return nil
 }
-func (s Air) HumidityRatio() float64 {
+func (s *Air) HumidityRatio() float64 {
 	return s.Humidity * 100 * s.AirDensity() / s.VapourDensity()
 }
 
-func (s Air) PartialPressure() float64 {
+func (s *Air) PartialPressure() float64 {
 	return PartialPressure(s.Temperature, s.HumidityRatio())
 }
